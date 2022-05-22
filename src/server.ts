ac78ab082afc8a80ae2cbd3c6b2fd5610356c371
Janowak536 <janowak536@gmail.com>
2022-05-22 17:50:46 +0200

    first commit

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import http from 'http';
+import mongoose from 'mongoose';
+import { config } from './config/config';
+
+const router = express();
+
+/**Connect to Mongo */
+
+mongoose.connect(config.mongo.url, {retryWrites:true,w:'majority'})
+.then(()=>{
+    console.log('connected');
+})
+.catch((error)=>{
+    console.log(error);
+});
